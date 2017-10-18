c4db1cddb75a7d0fb897b01856f5bff36e2faf5a
gzpeterz <zhangwen@gzpeter.com>
2017-10-18 14:16:11 +0800

    修改了 testJDBC.java

--- JDBC2/src/com/hc/test/testJDBC.java
+++ JDBC2/src/com/hc/test/testJDBC.java
@@ -17,6 +17,7 @@ public class testJDBC {
 		Connection conn = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/test", "peter", "peter");
 				*/
+		//  使用 DBUtil 类 来连接 数据库  
 		Connection conn = DBUtil.getConnection();
 		Statement stmt = conn.createStatement();
 		String sql = "select * from student";
